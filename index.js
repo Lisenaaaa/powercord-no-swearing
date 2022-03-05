21922efe14e92a6eb43d4796a7557f21441d9c26
David Nyan <DavidNyan211106@gmail.com>
2022-03-05 14:26:22 +0630

    This might work idk

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
 const { messages } = require("powercord/webpack");
+const words = require("./words").words;
 
 module.exports = class RemoveSwearWords extends Plugin {
-  async startPlugin() {
-    inject("removeSwearWords", messages, "sendMessage", (message) => {
-      const swearWords = ["fuck", "shit", "bitch", "crap", "cunt", "hell", "dumbass"];
+	async startPlugin() {
+		inject("removeSwearWords", messages, "sendMessage", (message) => {
+			const swearWords = words;
 
-      let swearsRemoved = 0;
+			let swearsRemoved = 0;
 
-      for (const word of swearWords) {
-        const regex = new RegExp(word, "gim");
-        if (regex.test(message[1].content)) {
-          swearsRemoved += message[1].content.match(regex).length;
-        }
+			for (const word of swearWords) {
+				const regex = new RegExp(word, "gim");
+				if (regex.test(message[1].content)) {
+					swearsRemoved += message[1].content.match(regex).length;
+				}
 
-        message[1].content = message[1].content.replaceAll(regex, "🤬");
-      }
+				message[1].content = message[1].content.replaceAll(regex, "🤬");
+			}
 
-      if (swearsRemoved >= 1) {
-        this.sendEphemeralMessage(
-          `Hey! Don't swear! I just had to remove ${swearsRemoved} bad words from that message, before I could send it.`
-        );
-      }
-    });
-  }
+			if (swearsRemoved >= 1) {
+				this.sendEphemeralMessage(
+					`Hey! Don't swear! I just had to remove ${swearsRemoved} bad words from that message, before I could send it.`
+				);
+			}
+		});
+	}
 
-  pluginWillUnload() {
-    uninject("removeSwearWords");
-  }
+	pluginWillUnload() {
+		uninject("removeSwearWords");
+	}
 
-  // this is from Little Furret#7901, https://canary.discord.com/channels/538759280057122817/755005784999329883/948454258603331584
-  getModule(name) {
-    var module;
-    webpackChunkdiscord_app.push([
-      [Math.random()],
-      {},
-      (r) => {
-        module =
-          module ||
-          Object.values(r.c).find(
-            (m) => m?.exports?.default && m.exports.default[name]
-          );
-      },
-    ]);
-    return module;
-  }
+	// this is from Little Furret#7901, https://canary.discord.com/channels/538759280057122817/755005784999329883/948454258603331584
+	getModule(name) {
+		var module;
+		webpackChunkdiscord_app.push([
+			[Math.random()],
+			{},
+			(r) => {
+				module =
+					module ||
+					Object.values(r.c).find(
+						(m) => m?.exports?.default && m.exports.default[name]
+					);
+			}
+		]);
+		return module;
+	}
 
-  // guess who basically made this! https://canary.discord.com/channels/538759280057122817/755015856945102891/948455712953106433
-  sendEphemeralMessage(content) {
-    this.getModule("sendMessage").exports.default.sendBotMessage(
-      this.getModule("getLastSelectedChannelId").exports.default.getChannelId(),
-      content
-    );
-  }
+	// guess who basically made this! https://canary.discord.com/channels/538759280057122817/755015856945102891/948455712953106433
+	sendEphemeralMessage(content) {
+		this.getModule("sendMessage").exports.default.sendBotMessage(
+			this.getModule(
+				"getLastSelectedChannelId"
+			).exports.default.getChannelId(),
+			content
+		);
+	}
 };
